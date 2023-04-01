68b2a92565cd1e60f8298e7098ff8f06a944f423
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-04-01 22:52:18 +0300

    createAccountInFirebase verification: createAccountBtn.setOnClickListener(v -> createAccount());

--- app/src/main/java/com/example/notespro/CreateAccountActivity.java
+++ app/src/main/java/com/example/notespro/CreateAccountActivity.java
@@ -1,14 +1,114 @@
 package com.example.notespro;
 
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAccountActivity extends AppCompatActivity {
+// create links
+    EditText emailEditText;
+    EditText passwordEditText;
+    EditText confirmPasswordEditText;
+    Button createAccountBtn;
+    ProgressBar progressBar;
+    TextView loginBtnTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        //set links
+        emailEditText = findViewById(R.id.set_email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        confirmPasswordEditText = findViewById(R.id.confirm_password_edit_text);
+        createAccountBtn = findViewById(R.id.create_account_button);
+        progressBar = findViewById(R.id.progress_bar);
+        loginBtnTextView = findViewById(R.id.login_text_view_btn);
+        // one BTN for registry
+        createAccountBtn.setOnClickListener(v -> createAccount());
+        loginBtnTextView.setOnClickListener(v -> finish());
+    }
+
+
+    /**
+     * Check info and do Registration
+     */
+    void createAccount() {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String confirmPassword = confirmPasswordEditText.getText().toString();
+
+        boolean isValidated = validateData(email, password, confirmPassword);
+        if (!isValidated) {
+            return;
+        }
+        createAccountInFirebase(email, password);
+    }
+
+    private void createAccountInFirebase(String email, String password) {
+        changeInProgress(true);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(CreateAccountActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        changeInProgress(false);
+                        if (task.isComplete()) {
+                            Toast.makeText(CreateAccountActivity.this, "YES", Toast.LENGTH_LONG).show();
+                            firebaseAuth.getCurrentUser().sendEmailVerification();
+                            firebaseAuth.signOut();
+                            finish();
+                        } else {
+                            Toast.makeText(CreateAccountActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    /**
+     * When user click on createAccountBtn connect to Firebase show spinner
+     * If no show BTN
+     */
+    void changeInProgress(boolean inProgress) {
+        if (inProgress) {
+            progressBar.setVisibility(View.VISIBLE);
+            createAccountBtn.setVisibility(View.GONE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+            createAccountBtn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * Checking is all field a right form
+     */
+    boolean validateData(String email, String password, String confirmPassword) {
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailEditText.setError("Email not valid");
+            return false;
+        }
+        if (password.length() < 4) {
+            passwordEditText.setError("Password to short");
+            return false;
+        }
+        if (!password.equals(confirmPassword)) {
+            confirmPasswordEditText.setError("Password not match");
+            return false;
+        }
+        return true;
     }
 }
