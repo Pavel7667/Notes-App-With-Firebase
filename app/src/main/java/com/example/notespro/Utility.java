a2a90f2adbb51d431dbc6df479600c8e04847409
pavel.hnelytsia <pavelgnelicya@gmail.com>
2023-04-02 14:05:15 +0300

    in Utility add Data convertor

--- app/src/main/java/com/example/notespro/Utility.java
+++ app/src/main/java/com/example/notespro/Utility.java
@@ -3,16 +3,18 @@ package com.example.notespro;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.SimpleDateFormat;
 import java.util.Collection;
 
 public class Utility {
     static void showToast(Context context, String message) {
-        Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
     }
 
     static CollectionReference getCollectionReferenceForNotes() {
@@ -22,4 +24,9 @@ public class Utility {
                 .document(currentUser.getUid())
                 .collection("my_notes");
     }
+
+    static String timestampToString(Timestamp timestamp) {
+        return new SimpleDateFormat("MM/dd/yyyy").format(timestamp.toDate());
+    }
 }
+
